601b44592a66ec8c4328404a1807218f94224c99
Tomás <tomasleggire@hotmail.com>
2023-11-26 21:05:59 -0300

    AppRoutes agregado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
 import "./App.scss";
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <h1>Home</h1> },
+    { path: "/facturacion", element: <h1>Facturacion</h1> },
+    { path: "/consulta", element: <h1>Consulta</h1> },
+    { path: "/*", element: <h1>Not Found</h1> },
+  ]);
+  return routes;
+};
 
 function App() {
   return (
-    <>
-      <div>Hola</div>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
